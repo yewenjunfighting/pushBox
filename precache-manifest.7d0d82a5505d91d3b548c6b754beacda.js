14dbc7ab41756f48bd133ec1a87513effd2afdd5
wenjun ye <1728914873@qq.com>
2019-04-09 21:03:22 +0800

    Updates

---
+++ precache-manifest.7d0d82a5505d91d3b548c6b754beacda.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "9b26917280eb4152cdfb13a60d292d04",
+    "url": "./static/media/indexPage.9b269172.jpg"
+  },
+  {
+    "revision": "28c033541b6c4910309f",
+    "url": "./static/css/main.ff150462.chunk.css"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "d4859eb5928e75773d58",
+    "url": "./static/js/2.731602fc.chunk.js"
+  },
+  {
+    "revision": "55b573f62ba115b350a777d2137e6d72",
+    "url": "./static/media/firecracker.55b573f6.png"
+  },
+  {
+    "revision": "a0850a91012e7f990126410983489e17",
+    "url": "./static/media/fireworks.a0850a91.jpg"
+  },
+  {
+    "revision": "28c033541b6c4910309f",
+    "url": "./static/js/main.ca9bc8ca.chunk.js"
+  },
+  {
+    "revision": "058f0cca37a099fca2612d37e28dc34d",
+    "url": "./static/media/level.058f0cca.jpg"
+  },
+  {
+    "revision": "367c60a2f730dc218c1ef642dbd04f8b",
+    "url": "./static/media/poster-about.367c60a2.jpg"
+  },
+  {
+    "revision": "ed0abcfa5610de86050e99f8e2919037",
+    "url": "./static/media/poster-drop-animate2.ed0abcfa.png"
+  },
+  {
+    "revision": "08f476345ac53c549bd39592111c7be2",
+    "url": "./static/media/poster-drop-animate1.08f47634.png"
+  },
+  {
+    "revision": "496d3ae0166d14ca1b124f3fdc6e80e1",
+    "url": "./static/media/tortoise.496d3ae0.png"
+  },
+  {
+    "revision": "c8358e69c91f46aa4378c80b28e022ae",
+    "url": "./index.html"
+  }
+];
